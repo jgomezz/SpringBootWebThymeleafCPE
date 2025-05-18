ebf3e02cf12d6ba55235e6429cce8f2fd66edfa9
jgomezz <jgomezz@gmail.com>
2025-05-18 10:07:45 -0500

    add home page

--- src/main/java/pe/edu/tecsup/tienda/webs/HelloWorldRestController.java
+++ src/main/java/pe/edu/tecsup/tienda/webs/HelloWorldRestController.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HomeRestController {
+public class HelloWorldRestController {
 
-    @GetMapping("/")
+    @GetMapping("/hello")
     public String home() {
         return "Hello, World Again!";
     }
