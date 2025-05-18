5752b0d6dffdefff926127c4a0c5fa455a7c7a6e
jgomezz <jgomezz@gmail.com>
2025-05-18 10:21:26 -0500

    add th:fragment

--- src/main/java/pe/edu/tecsup/tienda/webs/HomeController.java
+++ src/main/java/pe/edu/tecsup/tienda/webs/HomeController.java
@@ -1,13 +1,18 @@
 package pe.edu.tecsup.tienda.webs;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+
+        model.addAttribute("mensaje",
+                "Bienvenido a Spring Boot con  Thymeleaf");
+
         return "home";
     }
 
