790c37007ef9dbef734c7ab87ba678e5ad5a2c7c
jgomezz <jgomezz@gmail.com>
2025-05-18 09:12:38 -0500

    first  controller

--- src/main/java/pe/edu/tecsup/tienda/webs/ProductoController.java
+++ src/main/java/pe/edu/tecsup/tienda/webs/ProductoController.java
@@ -1,4 +1,18 @@
 package pe.edu.tecsup.tienda.webs;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class ProductoController {
+
+    /**
+     * Muestra la lista de productos
+     * @return
+     */
+    @GetMapping("/productos")
+    public String index() {
+        return "productos";
+    }
+
 }
