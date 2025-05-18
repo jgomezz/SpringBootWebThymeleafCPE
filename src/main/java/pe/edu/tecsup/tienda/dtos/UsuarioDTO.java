460c21d8c396550762141a8e452a228278096656
jgomezz <jgomezz@gmail.com>
2025-05-18 10:52:10 -0500

    include form

--- src/main/java/pe/edu/tecsup/tienda/dtos/UsuarioDTO.java
+++ src/main/java/pe/edu/tecsup/tienda/dtos/UsuarioDTO.java
@@ -1,5 +1,9 @@
 package pe.edu.tecsup.tienda.dtos;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,7 +13,16 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class UsuarioDTO {
 
+    private Long id;
+
+    @NotBlank(message = "Nombre es requerido")
+    @Size(min = 2, max = 5, message = "El nombre debe tener entre 2 y 5 characters")
     private String nombre;
+
+    @NotBlank(message = "Apelldos es requerido")
     private String apellidos;
 
+    @Min(value = 18, message = "Age must be at least 18")
+    @Max(value = 120, message = "Age must be less than 120")
+    private Integer edad;
 }
--- src/main/java/pe/edu/tecsup/tienda/webs/UsuarioController.java
+++ src/main/java/pe/edu/tecsup/tienda/webs/UsuarioController.java
@@ -1,8 +1,12 @@
 package pe.edu.tecsup.tienda.webs;
 
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import pe.edu.tecsup.tienda.dtos.UsuarioDTO;
 
 @Controller
@@ -13,6 +17,21 @@ public class UsuarioController {
 
         model.addAttribute("usuario",
                 new UsuarioDTO());
+
+        return "registrar";
+    }
+
+
+    @PostMapping("/registrar")
+    public String registrar(@Valid @ModelAttribute("usuario") UsuarioDTO usuario,
+                            BindingResult result,
+                            Model model) {
+        // Validar el formulario
+        if (result.hasErrors()) {
+            model.addAttribute("message", "Errores!");
+        } else {
+            model.addAttribute("message", "Registro exitoso!");
+        }
 
         return "registrar";
     }
