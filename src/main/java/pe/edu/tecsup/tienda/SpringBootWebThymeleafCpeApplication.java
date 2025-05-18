ca5dcfbbb8224170f1c4a8962c5cc39d80417958
jgomezz <jgomezz@gmail.com>
2025-05-18 08:42:57 -0500

    first version

---
+++ src/main/java/pe/edu/tecsup/tienda/SpringBootWebThymeleafCpeApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.tecsup.tienda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebThymeleafCpeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebThymeleafCpeApplication.class, args);
+	}
+
+}
